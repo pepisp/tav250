868dcf14be43d423ea0593a2b748baf8d335086a
PPiotrowski <piotr.piotrowski@pwr.edu.pl>
2017-09-22 12:00:32 +0200

    Pierwsza wersja z niepełnymi funkcjonalnościami

--- tav250.py
+++ tav250.py
@@ -9,7 +9,7 @@ _SUFFIX=b'\r\n'
 
 class TAV250(object):
     def __init__(self,device='/dev/ttyUSB0'):
-        self.sp=serial.Serial(device,57600,timeout=2)
+        self.sp=serial.Serial(device,57600,timeout=1)
         self.onAir
  
     def __del__(self):
@@ -18,38 +18,45 @@ class TAV250(object):
     def __sendCmd(self,cmd):
         """Function for writing message to Tav device. It returns bare responce payload."""
         cmd=_PREFIX+cmd+_SUFFIX
-        print(cmd)
+#        print(cmd)
         self.sp.write(cmd)
         if int(cmd[4:6])>49:	#if write command then there is no response
-            print('Sending write command... No response')
-#            return ''
+#            print('Sending write command '+cmd[4:6]+' No response...')
+            return '00'
+        sleep(2)
+#        print("if it was command i'm not supose to be here")
         resp=self.sp.readline()
         return resp[9:-3]
     @property
     def onAir(self):
-        print("i'm getter")
-        resp=self.__sendCmd('45')
+#        print("i'm getter")
+        resp=self.__sendCmd(b'45')
         self.__isOnAir=False
-        print(resp)
+#        print(resp)
         if int(resp,16)&1:
             self.__isOnAir=True
         return self.__isOnAir
     @onAir.setter
-    def onAir(self,isOnAir=True):
-        print("i' in setter")
-	if isOnAir:
-            print("Lets go onAir!!!")
-            self.__sendCmd(b'5003')
-            self.__isOnAir=True
-        else:
-            print('lets go offline')
-            self.__sendCmd('5002')
-            self.__isOnAir=False
-	return self.__isOnAir
+    def onAir(self,isOnAir):
+#        print("i' in setter")
+        for atempt in range(1,5):
+            if isOnAir:
+#                print("Lets go onAir!!!")
+                self.__sendCmd(b'5003')
+#                self.__isOnAir=True
+            else:
+#                print('lets go offline')
+                self.__sendCmd(b'5002')
+#                self.__isOnAir=False
+            sleep(1)
+            if self.onAir==isOnAir:
+                return True
+
+        return False
 	
     def GetPowerMeasures(self):
         """This function returns power mesuremetns related to the RF"""
-        powerMeasures=self.__sendCmd('40')
+        powerMeasures=self.__sendCmd(b'40')
         print(powerMeasures)
         FwdPwr=int(powerMeasures[0:4],16)*0.1
         RefPwr=int(powerMeasures[4:8],16)*0.1
@@ -59,7 +66,7 @@ class TAV250(object):
 
     def GetCurrentMeasures(self):
         """This function returns currents on PA1-4 """
-        currMeasures=self.__sendCmd('41')
+        currMeasures=self.__sendCmd(b'41')
         currents=[]
         for PA in range(0,9,2):
             currents.append(currMeasures[PA:PA+2])
@@ -67,13 +74,13 @@ class TAV250(object):
 
     def GetVoltageMeasures(self):
         """This function returns voltages on PA1-4 """
-        currMeasures=self.__sendCmd('42')
+        currMeasures=self.__sendCmd(b'42')
         currents=[]
         for PA in range(0,9,2):
             currents.append(currMeasures[PA:PA+2])
         return currents
     def GetTempMeasures(self):
-        temps=self.__sendCmd('43')
+        temps=self.__sendCmd(b'43')
         print(temps)
         return [int(temps[0:4],16)*0.1,int(temps[4:],16)*0.1]
 
@@ -81,7 +88,7 @@ class TAV250(object):
         """Function returns active alarms
         TODO: Check bit order 
         """
-        alarms=self.__sendCmd('44')
+        alarms=self.__sendCmd(b'44')
         return alarms
 if __name__=='__main__':
     tav=TAV250()
@@ -93,9 +100,6 @@ if __name__=='__main__':
 #    print(tav.GetAlarms())
 
     tav.onAir=True	    
-    tav.onAir=True	    
-    print(tav.onAir)	    
-
     sleep(15)
     tav.onAir=False
     print(tav.onAir)	    
